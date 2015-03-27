9455c4a0d1fa108b543d507e6186d05f0f20f2a0
Connor Abdelnoor <connor.abd@gmail.com>
2015-03-27 12:01:12 -0700

    Create crud methods for all stores with routes and twig files.

--- app/app.php
+++ app/app.php
@@ -39,7 +39,56 @@
         return $app['twig']->render('stores.html.twig', array('stores' => Store::getAll()));
     });
 // Single Store Routes
+    $app->get('/store/{id}', function($id) use($app){
+        $store = Store::find($id);
+        $store_brands = $store->getBrands();
 
+        return $app['twig']->render('store.html.twig', array('store' => $store, 'brands' => $store_brands));
+    });
+
+    $app->post('/store_add_brand/{id}', function($id) use($app){
+        $store = Store::find($id);
+        $name = $_POST['name'];
+        $new_brand = new Brand($name);
+        $new_brand->save();
+        $store->addBrand($new_brand);
+        $store_brands = $store->getBrands();
+
+        return $app['twig']->render('store.html.twig', array('store' => $store, 'brands' => $store_brands));
+    });
+
+    $app->post('/delete_store_brands/{id}', function($id) use($app){
+        $store = Store::find($id);
+    // Only delete brands in this store
+        foreach($store->getBrands() as $shoe){
+            $shoe->delete();
+        }
+        $store_brands = $store->getBrands();
+
+        return $app['twig']->render('store.html.twig', array('store' => $store, 'brands' => $store_brands));
+    });
+
+    $app->get('/edit_store/{id}', function($id) use($app){
+        $store = Store::find($id);
+
+        return $app['twig']->render('store_edit.html.twig', array('store' => $store));
+    });
+    $app->patch('/edit_store/{id}', function($id) use($app){
+        $store = Store::find($id);
+        $new_name = $_POST['name'];
+        $store->update($new_name);
+        $store_brands = $store->getBrands();
+
+        return $app['twig']->render('store.html.twig', array('store' => $store, 'brands' => $store_brands));
+    });
+
+    $app->delete('/delete_store/{id}', function($id) use($app){
+        $store = Store::find($id);
+        $store->delete();
+
+        return $app['twig']->render('stores.html.twig', array('stores' => Store::getAll()));
+    });
+// All brand routes
 
 
     return $app;
