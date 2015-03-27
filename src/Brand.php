62920bbfb31084d004cd7beb843dccc16fc9e52f
Connor Abdelnoor <connor.abd@gmail.com>
2015-03-27 09:43:54 -0700

    Finish creating brands class.

--- src/Brand.php
+++ src/Brand.php
@@ -49,6 +49,29 @@
             $GLOBALS->exec("DELETE FROM brands WHERE id = {$this->getId()};");
         }
 
+        function addStore($store)
+        {
+            $GLOBALS['DB']->exec("INSERT INTO brands_stores (brand_id, store_id) VALUES ({$this->getId()}, {$store->getId()});");
+        }
+
+        function getStores()
+        {
+            $query = $GLOBALS['DB']->query("SELECT stores.* FROM
+                brands JOIN brands_stores ON (brands.id = brands_stores.brand_id)
+                JOIN stores ON (brands_stores.store_id = stores.id)
+                WHERE brands.id = {$this->getId()};");
+            $brands = $query->fetchAll(PDO::FETCH_ASSOC);
+
+            $found_brands = array();
+            foreach($brands as $shoe){
+                $name = $shoe['name'];
+                $id = $shoe['id'];
+                $new_brand = new Brand($name, $id);
+                array_push($found_brands, $new_brand);
+            }
+            return $found_brands;
+        }
+
         static function getAll()
         {
             $statement = $GLOBALS['DB']->query("SELECT * FROM brands;");
